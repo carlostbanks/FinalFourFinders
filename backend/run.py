10892b76f4446b74b990e4fa083949b52704c953
Carlos Banks <carlos.banks@spatialfront.com>
2025-03-28 11:33:39 -0400

    add files for Render

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 # Load environment variables
 load_dotenv()
@@ -7,4 +8,5 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
